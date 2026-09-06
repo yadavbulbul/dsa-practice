94ce81b0fb8b99b2e78b1e706071ca616d6163ff
Bulbul Yadav <yadavpihu294@gmail.com>
2026-09-06 21:43:15 +0530

    Update 32-ninja'sTraining.cpp

--- Day5/DP/32-ninja'sTraining.cpp
+++ Day5/DP/32-ninja'sTraining.cpp
@@ -32,3 +32,35 @@ class Solution {
         return dp[day][last]=maxi;
     }
 };
+
+/*
+class Solution {
+  public:
+    int maximumPoints(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        vector<vector<int>>dp(n,vector<int>(4,0));
+        //dp[day][last]
+        dp[0][0]= max(mat[0][1],mat[0][2]);
+        dp[0][1]= max(mat[0][0],mat[0][2]);
+        dp[0][2]= max(mat[0][1],mat[0][0]);
+        dp[0][3]= max(mat[0][0],max(mat[0][1],mat[0][2]));
+        
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                
+                dp[day][last]=0;
+                for(int task=0;task<3;task++){
+                    if(task!=last){
+                        int point = mat[day][task]+dp[day-1][task];
+                        dp[day][last]=max(dp[day][last],point);
+                    }
+                    
+                }
+            }
+        }
+        return dp[n-1][3];
+    }
+    
+};
+*/
