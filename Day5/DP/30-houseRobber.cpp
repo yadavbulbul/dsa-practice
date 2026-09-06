a61d4d55a17fdb14119a3597aa5d8d79b7efc3d5
Bulbul Yadav <yadavpihu294@gmail.com>
2026-09-06 16:47:36 +0530

    tabulation

--- Day5/DP/30-houseRobber.cpp
+++ Day5/DP/30-houseRobber.cpp
@@ -45,3 +45,23 @@ public:
         return dp[index]=max(rob,notRob);
     }
 };
+
+/*
+int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> dp(n);
+
+        dp[0] = nums[0];
+
+        if(n > 1)
+            dp[1] = max(nums[0], nums[1]);
+        for(int i=2;i<nums.size();i++){
+            int rob = nums[i]+dp[i-2];
+            int notRob = dp[i-1];
+            dp[i]=max(rob,notRob);
+        }
+        return dp[n-1];
+
+    }
+*/
